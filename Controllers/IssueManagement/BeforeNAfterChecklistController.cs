89e0e89e3159c75671562208f7f5602d5b933888
clement <clementykm-aa15@student.tarc.edu.my>
2019-12-14 21:43:33 +0800

    issue management

---
+++ Controllers/IssueManagement/BeforeNAfterChecklistController.cs
@@ -0,0 +1,32 @@
+﻿using KJCFRubberRoller.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KJCFRubberRoller.Controllers
+{
+    public class BeforeNAfterChecklistController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        // Constructor
+        public BeforeNAfterChecklistController()
+        {
+            _db = new ApplicationDbContext();
+        }
+
+        // Add database dispose
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _db.Dispose();
+        }
+        // GET: BeforeNAfterChecklist
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/IssueManagement/ScheduleController.cs
@@ -0,0 +1,32 @@
+﻿using KJCFRubberRoller.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KJCFRubberRoller.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        // Constructor
+        public ScheduleController()
+        {
+            _db = new ApplicationDbContext();
+        }
+
+        // Add database dispose
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _db.Dispose();
+        }
+        // GET: Schedule
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
