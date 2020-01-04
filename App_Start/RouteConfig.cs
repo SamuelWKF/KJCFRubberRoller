9c1516de5ada9a671946f778c579b8554fd7dcd1
TK1108 <44633987+TK1108@users.noreply.github.com>
2020-01-04 13:13:33 +0800

    Included home and manage controller back
    added maintenance controller for my part

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -21,6 +21,8 @@ namespace KJCFRubberRoller
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+          
         }
     }
 }
---
+++ Controllers/MaintenanceReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KJCFRubberRoller.Controllers
+{
+    public class MaintenanceReportController : Controller
+    {
+        // GET: MaintenanceReport
+        public string Index()
+        {
+            return "This is index action" ;
+        }
+    }
+}
