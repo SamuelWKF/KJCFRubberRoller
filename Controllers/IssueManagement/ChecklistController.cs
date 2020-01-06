e039d44e2fbf5513d66654d36335979e2d20c7e6
clement <clementykm-aa15@student.tarc.edu.my>
2020-01-06 13:04:23 +0800

    change ui

---
+++ Controllers/IssueManagement/ChecklistController.cs
@@ -0,0 +1,75 @@
+﻿using KJCFRubberRoller.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KJCFRubberRoller.Controllers
+{
+    public class ChecklistController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public ChecklistController()
+        {
+            _db = new ApplicationDbContext();
+        }
+
+        // Add database dispose
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _db.Dispose();
+        }
+
+        public ActionResult BeforeChecklistCreate()
+        {
+            return View("BeforeChecklistCreate");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult BeforeChecklistCreate(BeforeRollerIssueChecklist beforeRollerIssueChecklist)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.beforeRollerIssueChecklists.Add(beforeRollerIssueChecklist);
+                _db.SaveChanges();
+                TempData["saveStatus"] = true;
+                TempData["saveStatusMsg"] = "New Before Roller Issue has been successfully added!";
+                return Redirect(Request.UrlReferrer.ToString());
+            }
+
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+
+        public ActionResult AfterChecklistCreate()
+        {
+            return View("AfterChecklistCreate");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AfterChecklistCreate(AfterRollerProductionChecklist afterChecklistCreate)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.afterRollerProductionChecklists.Add(afterChecklistCreate);
+                _db.SaveChanges();
+                TempData["saveStatus"] = true;
+                TempData["saveStatusMsg"] = "New After Production Checklist has been successfully added!";
+                return Redirect(Request.UrlReferrer.ToString());
+            }
+
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+
+
+        // GET: Checklist
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
