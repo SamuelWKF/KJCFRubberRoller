122527955fdd63eaf84a8256973cbc2d6eb215ec
TK1108 <44633987+TK1108@users.noreply.github.com>
2020-01-17 00:58:54 +0800

    Minor changes made but not complete

--- Controllers/MaintenanceReportController.cs
+++ Controllers/MaintenanceReportController.cs
@@ -37,35 +37,29 @@ namespace KJCFRubberRoller.Controllers
         // GET: Returns create form
         public ActionResult Create()
         {
-            LogAction.log(this._controllerName, "GET", "Requested RubberRoller-Create webpage", User.Identity.GetUserId());
-
-        
+            LogAction.log(this._controllerName, "GET", "Requested MaintenanceReport-Create webpage", User.Identity.GetUserId());
+            
             return View("CreateEditMaintenanceRecord");
         }
 
-        // POST: Create new rubber roller record
+        // POST: Create new rubber roller Maintenance record
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(RubberRoller rubberRoller)
+        public ActionResult Create(Maintenance maintenance)
         {
             // Check if roller ID exist from DB
-            var dbRoller = _db.rubberRollers.Where(r => r.rollerID == rubberRoller.rollerID).FirstOrDefault();
+            var dbRoller = _db.maintenances.Where(m => m.maintenanceID == maintenance.maintenanceID).FirstOrDefault();
             if (dbRoller != null)
-                ModelState.AddModelError("rollerID", "There is already an existing roller with the same roller ID.");
+                ModelState.AddModelError("maintenanceID", "There is already an existing report with same ID.");
 
             if (!ModelState.IsValid || dbRoller != null)
             {
-                
-                return View("CreateEditForm", rubberRoller);
+               
+                return View("CreateEditMaintenanceRecord", maintenance);
             }
 
-            _db.rubberRollers.Add(rubberRoller);
+            _db.maintenances.Add(maintenance);
             int result = _db.SaveChanges();
-
-            //if (rubberRoller.supplier.Equals("Canco"))
-            //{
-            //    return RedirectToAction("CancoChecklist", new { rubberRoller.id });
-            //}
 
             if (result > 0)
             {
@@ -80,6 +74,57 @@ namespace KJCFRubberRoller.Controllers
             return Redirect(Request.UrlReferrer.ToString());
         }
 
+        // POST: Update existing rubber roller record
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(Maintenance maintenance)
+        {
+            try
+            {
+                // Retrieve existing specific rubber roller from database
+                Maintenance maintenances = _db.maintenances.SingleOrDefault(m => m.maintenanceID == maintenance.maintenanceID);
+
+                if (maintenances == null)
+                    return RedirectToAction("Index");
+
+                // Update rubber roller details
+                if (maintenances.rollerID != maintenance.rollerID)
+                    maintenances.rollerID = maintenance.rollerID;
+                maintenances.diameterCore = maintenance.diameterCore;
+                maintenances.diameterRoller = maintenance.diameterRoller;
+                maintenances.totalMileage = maintenance.totalMileage;
+                maintenances.openingStockDate = maintenance.openingStockDate;
+                maintenances.lastProductionLine = maintenance.lastProductionLine;
+                maintenances.reason = maintenance.reason;
+                maintenances.remark = maintenance.remark;
+                maintenances.newDiameter = maintenance.newDiameter;
+                maintenances.newShoreHardness = maintenance.newShoreHardness;
+                maintenances.correctiveAction = maintenance.correctiveAction;
+                maintenances.reportedBy = maintenance.reportedBy;
+                maintenances.reportDateTime = maintenance.reportDateTime;
+
+                int result = _db.SaveChanges();
+
+                //if (rubberRoller.supplier.Equals("Canco") && notCancoRoller)
+                //    return RedirectToAction("CancoChecklist", new { rubberRoller.id });
+
+                if (result > 0)
+                {
+                    LogAction.log(this._controllerName, "POST", "Updated roller record", User.Identity.GetUserId());
+                    TempData["formStatus"] = true;
+                    TempData["formStatusMsg"] = "Rubber roller details has been successfully updated!";
+                }
+
+                return Redirect(Request.UrlReferrer.ToString());
+            }
+            catch (Exception ex)
+            {
+                LogAction.log(this._controllerName, "POST", "Error: " + ex.Message, User.Identity.GetUserId());
+                TempData["formStatus"] = false;
+                TempData["formStatusMsg"] = "Oops! Something went wrong. The rubber roller has not been successfully updated.";
+                return Redirect(Request.UrlReferrer.ToString());
+            }
+        }
 
 
     }
