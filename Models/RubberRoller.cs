3c6a07d486fb796341f8b7afd322bf3c6398bcd6
TK1108 <44633987+TK1108@users.noreply.github.com>
2020-03-09 13:09:16 +0800

    deleted customevalidation

--- Models/RubberRoller.cs
+++ Models/RubberRoller.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
-using KJCFRubberRoller.CustomValidations;
+//using KJCFRubberRoller.CustomValidations;
 
 namespace KJCFRubberRoller.Models
 {
