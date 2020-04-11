99fb546285de8f5402a6a53ecbe88f2e81df43ba
StarmanW <swkf3011@hotmail.com>
2020-04-12 04:30:52 +0800

    Update

---
+++ Migrations/202004111008145_InitialMigration.cs
@@ -0,0 +1,296 @@
+﻿namespace KJCFRubberRoller.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.AfterRollerProductionChecklists",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        scheduleID = c.Int(nullable: false),
+                        dateTime = c.DateTime(nullable: false),
+                        rollerAppearance = c.String(nullable: false, maxLength: 100),
+                        rollerSendTo = c.String(nullable: false, maxLength: 100),
+                        remarks = c.String(maxLength: 255),
+                        preparedBy_Id = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.id)
+                .ForeignKey("dbo.AspNetUsers", t => t.preparedBy_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Schedules", t => t.scheduleID, cascadeDelete: true)
+                .Index(t => t.scheduleID)
+                .Index(t => t.preparedBy_Id);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        staffID = c.String(nullable: false, maxLength: 5),
+                        name = c.String(nullable: false, maxLength: 255),
+                        IC = c.String(nullable: false, maxLength: 12),
+                        position = c.Int(nullable: false),
+                        status = c.Int(nullable: false),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.Schedules",
+                c => new
+                    {
+                        scheduleID = c.Int(nullable: false, identity: true),
+                        operationLine = c.Int(nullable: false),
+                        rollerID = c.Int(nullable: false),
+                        startDateTime = c.DateTime(nullable: false),
+                        endDateTime = c.DateTime(),
+                        product = c.String(nullable: false, maxLength: 255),
+                        tinplateSize = c.String(maxLength: 255),
+                        quantity = c.Int(nullable: false),
+                        startMileage = c.Int(nullable: false),
+                        endMileage = c.Int(nullable: false),
+                        remark = c.String(maxLength: 255),
+                        status = c.Int(nullable: false),
+                        RubberRoller_id = c.Int(),
+                    })
+                .PrimaryKey(t => t.scheduleID)
+                .ForeignKey("dbo.RubberRollers", t => t.RubberRoller_id)
+                .Index(t => t.RubberRoller_id);
+            
+            CreateTable(
+                "dbo.BeforeRollerIssueChecklists",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        scheduleID = c.Int(nullable: false),
+                        dateTime = c.DateTime(nullable: false),
+                        rollerSH = c.String(nullable: false, maxLength: 100),
+                        rollerRoundness = c.String(nullable: false, maxLength: 100),
+                        hubsCondition = c.String(nullable: false, maxLength: 100),
+                        nutUsed = c.String(nullable: false, maxLength: 100),
+                        preparedBy_Id = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.id)
+                .ForeignKey("dbo.AspNetUsers", t => t.preparedBy_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Schedules", t => t.scheduleID, cascadeDelete: true)
+                .Index(t => t.scheduleID)
+                .Index(t => t.preparedBy_Id);
+            
+            CreateTable(
+                "dbo.RubberRollers",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        rollerCategoryID = c.Int(nullable: false),
+                        rollerID = c.String(nullable: false, maxLength: 15),
+                        type = c.String(nullable: false, maxLength: 100),
+                        usage = c.String(nullable: false, maxLength: 255),
+                        shoreHardness = c.String(nullable: false, maxLength: 100),
+                        diameter = c.Double(nullable: false),
+                        depthOfGroove = c.String(nullable: false, maxLength: 100),
+                        condition = c.String(nullable: false, maxLength: 255),
+                        last_usage_date = c.DateTime(),
+                        opening_stock_date = c.DateTime(),
+                        remark = c.String(maxLength: 255),
+                        isRefurbished = c.Boolean(nullable: false),
+                        status = c.String(nullable: false, maxLength: 100),
+                    })
+                .PrimaryKey(t => t.id)
+                .ForeignKey("dbo.RollerCategories", t => t.rollerCategoryID, cascadeDelete: true)
+                .Index(t => t.rollerCategoryID);
+            
+            CreateTable(
+                "dbo.Maintenances",
+                c => new
+                    {
+                        maintenanceID = c.Int(nullable: false, identity: true),
+                        rollerID = c.Int(nullable: false),
+                        title = c.String(nullable: false),
+                        reportDateTime = c.DateTime(nullable: false),
+                        approveDateTime = c.DateTime(),
+                        diameterCore = c.Double(nullable: false),
+                        diameterRoller = c.Double(nullable: false),
+                        totalMileage = c.Int(nullable: false),
+                        openingStockDate = c.DateTime(),
+                        lastProductionLine = c.String(),
+                        reason = c.String(nullable: false, maxLength: 255),
+                        remark = c.String(maxLength: 255),
+                        newDiameter = c.Double(nullable: false),
+                        sendTo = c.String(nullable: false),
+                        newShoreHardness = c.String(nullable: false, maxLength: 255),
+                        correctiveAction = c.String(maxLength: 255),
+                        status = c.Int(nullable: false),
+                        sendForRefurbished = c.Boolean(nullable: false),
+                        statusRemark = c.String(),
+                        imagePath = c.String(),
+                        reportedBy_Id = c.String(nullable: false, maxLength: 128),
+                        RubberRoller_id = c.Int(),
+                        verfiedBy_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.maintenanceID)
+                .ForeignKey("dbo.AspNetUsers", t => t.reportedBy_Id, cascadeDelete: true)
+                .ForeignKey("dbo.RubberRollers", t => t.RubberRoller_id)
+                .ForeignKey("dbo.AspNetUsers", t => t.verfiedBy_Id)
+                .Index(t => t.reportedBy_Id)
+                .Index(t => t.RubberRoller_id)
+                .Index(t => t.verfiedBy_Id);
+            
+            CreateTable(
+                "dbo.RollerCategories",
+                c => new
+                    {
+                        rollerCategoryID = c.Int(nullable: false, identity: true),
+                        size = c.String(nullable: false, maxLength: 255),
+                        description = c.String(nullable: false, maxLength: 255),
+                        minAmount = c.Int(nullable: false),
+                        remark = c.String(maxLength: 255),
+                        criticalStatus = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.rollerCategoryID);
+            
+            CreateTable(
+                "dbo.RollerLocations",
+                c => new
+                    {
+                        rollerLocationID = c.Int(nullable: false, identity: true),
+                        rollerID = c.Int(nullable: false),
+                        location = c.String(maxLength: 200),
+                        operationLine = c.Int(nullable: false),
+                        dateTimeIn = c.DateTime(),
+                        dateTimeOut = c.DateTime(),
+                        RubberRoller_id = c.Int(),
+                    })
+                .PrimaryKey(t => t.rollerLocationID)
+                .ForeignKey("dbo.RubberRollers", t => t.RubberRoller_id)
+                .Index(t => t.RubberRoller_id);
+            
+            CreateTable(
+                "dbo.Logs",
+                c => new
+                    {
+                        logID = c.Int(nullable: false, identity: true),
+                        dateTime = c.DateTime(nullable: false),
+                        controller = c.String(nullable: false, maxLength: 255),
+                        action = c.String(nullable: false, maxLength: 255),
+                        description = c.String(nullable: false, maxLength: 255),
+                        staffID_Id = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.logID)
+                .ForeignKey("dbo.AspNetUsers", t => t.staffID_Id, cascadeDelete: true)
+                .Index(t => t.staffID_Id);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.Logs", "staffID_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Schedules", "RubberRoller_id", "dbo.RubberRollers");
+            DropForeignKey("dbo.RollerLocations", "RubberRoller_id", "dbo.RubberRollers");
+            DropForeignKey("dbo.RubberRollers", "rollerCategoryID", "dbo.RollerCategories");
+            DropForeignKey("dbo.Maintenances", "verfiedBy_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.Maintenances", "RubberRoller_id", "dbo.RubberRollers");
+            DropForeignKey("dbo.Maintenances", "reportedBy_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.BeforeRollerIssueChecklists", "scheduleID", "dbo.Schedules");
+            DropForeignKey("dbo.BeforeRollerIssueChecklists", "preparedBy_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AfterRollerProductionChecklists", "scheduleID", "dbo.Schedules");
+            DropForeignKey("dbo.AfterRollerProductionChecklists", "preparedBy_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.Logs", new[] { "staffID_Id" });
+            DropIndex("dbo.RollerLocations", new[] { "RubberRoller_id" });
+            DropIndex("dbo.Maintenances", new[] { "verfiedBy_Id" });
+            DropIndex("dbo.Maintenances", new[] { "RubberRoller_id" });
+            DropIndex("dbo.Maintenances", new[] { "reportedBy_Id" });
+            DropIndex("dbo.RubberRollers", new[] { "rollerCategoryID" });
+            DropIndex("dbo.BeforeRollerIssueChecklists", new[] { "preparedBy_Id" });
+            DropIndex("dbo.BeforeRollerIssueChecklists", new[] { "scheduleID" });
+            DropIndex("dbo.Schedules", new[] { "RubberRoller_id" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
+            DropIndex("dbo.AfterRollerProductionChecklists", new[] { "preparedBy_Id" });
+            DropIndex("dbo.AfterRollerProductionChecklists", new[] { "scheduleID" });
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.Logs");
+            DropTable("dbo.RollerLocations");
+            DropTable("dbo.RollerCategories");
+            DropTable("dbo.Maintenances");
+            DropTable("dbo.RubberRollers");
+            DropTable("dbo.BeforeRollerIssueChecklists");
+            DropTable("dbo.Schedules");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
+            DropTable("dbo.AfterRollerProductionChecklists");
+        }
+    }
+}
